881333d8c651644b241010f3b73f20f17d47328c
Aaron Gough <aaron@aarongough.com>
2010-06-04 12:33:35 -0400

    Basic working version of code

---
+++ lib/deepopenstruct.rb
@@ -0,0 +1,23 @@
+class DeepOpenStruct
+  def self.load(item)
+    raise ArgumentError, "DeepOpenStruct must be passed a Hash or Array" unless(item.is_a?(Hash) || item.is_a?(Array))
+    if(item.is_a?(Hash))
+      item.merge!(item) do |key, value, oldvalue|
+        if(value.is_a?(Hash) || value.is_a?(Array))
+          DeepOpenStruct.load(value)
+        else
+          value
+        end
+      end
+      return OpenStruct.new(item)
+    elsif(item.is_a?(Array))
+      return item.map do |value|
+        if(value.is_a?(Hash) || value.is_a?(Array))
+          DeepOpenStruct.load(value)
+        else
+          value
+        end
+      end
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,17 @@
 require 'rubygems'
 require 'test/unit'
+require 'ostruct'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'deepopenstruct'
+
+require_files = []
+require_files.concat Dir[File.join(File.dirname(__FILE__), 'setup', '*.rb')]
+
+require_files.each do |file|
+  require File.expand_path(file)
+end
 
 class Test::Unit::TestCase
 end
